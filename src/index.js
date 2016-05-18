a658f6d29bcb0c62679e45f4ea1d9f78e68b25af
Jonathan Gertig <jcgertig@gmail.com>
2016-05-17 23:11:43 -0400

    fix default ternary

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const DatePicker = React.createClass({
   },
   render() {
     const props = this.props;
-    const date = typeof props.date !== 'undefined' new Date(props.date) : props.defaultDate;
+    const date = typeof props.date !== 'undefined' ? new Date(props.date) : props.defaultDate;
     const minDate = typeof props.minDate !== 'undefined' ? new Date(props.minDate) : props.minDate;
     const maxDate = typeof props.maxDate !== 'undefined' ? new Date(props.maxDate) : props.maxDate;
     return (
