d33a3fadc6b10af2f09ee4deedfa796fc261fcfa
Jonathan Gertig <jcgertig@gmail.com>
2016-05-17 23:07:10 -0400

    Fixed for required props

--- src/index.js
+++ src/index.js
@@ -16,15 +16,20 @@ const DatePicker = React.createClass({
   },
   render() {
     const props = this.props;
-    const date = props.date || props.defaultDate;
-    return (<DatePickerIOS
-          date={date}
-          mode={props.mode}
-          onDateChange={this.onDateChange}
-          minimumDate={props.minDate}
-          maximumDate={props.maxDate}
-          minuteInterval={props.minuteInterval}
-          timeZoneOffsetInMinutes={props.timeZoneOffsetInMinutes}/>);
+    const date = typeof props.date !== 'undefined' new Date(props.date) : props.defaultDate;
+    const minDate = typeof props.minDate !== 'undefined' ? new Date(props.minDate) : props.minDate;
+    const maxDate = typeof props.maxDate !== 'undefined' ? new Date(props.maxDate) : props.maxDate;
+    return (
+      <DatePickerIOS
+        date={date}
+        mode={props.mode}
+        onDateChange={this.onDateChange}
+        minimumDate={minDate}
+        maximumDate={maxDate}
+        minuteInterval={props.minuteInterval}
+        timeZoneOffsetInMinutes={props.timeZoneOffsetInMinutes}
+      />
+    );
   },
 });
 
